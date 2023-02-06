dfb62f339ddd7d37b837cf94f60835c0e98f9f0e
Luciano Cleistenes <123988661+Cleistenes@users.noreply.github.com>
2023-02-06 10:53:17 +0000

    Add files via upload
    This is just an update with the contributions given by

--- comandosdedecisao.java
+++ comandosdedecisao.java
@@ -1,22 +1,22 @@
 public class comandosdedecisao {
     public static void main(String[] args){
         int prova1 = 55;
-		int prova2 = 60;
-		int prova3 = 90;
-		int media = (prova1 + prova2 + prova3) / 3;
-		int resultadofinal = media;
-		int faltas = 15;
-		int maxFaltas = 12;
-		System.out.println("Resultado final: " + resultadofinal + " quantidade de faltas: " + faltas);
-		if (resultadofinal >= 65 && faltas < maxFaltas) {
+        int prova2 = 45;
+        int prova3 = 30;
+        int media = (prova1+prova2+prova3) / 3;
+        int resultadofinal = media;
+        int faltas = 9;
+        int maxFaltas = 12;
+        System.out.println("Resultado final: " + resultadofinal + " Quantidade de faltas: " + faltas);
+        if (resultadofinal >= 65 && faltas < maxFaltas) {
 			System.out.println("Aprovado!");
-		} else if (resultadofinal >= 40  && faltas < maxFaltas) {
-			System.out.println("Recuperacao por média");
-		} else if(faltas > maxFaltas){
-			System.out.println("Reprovado por falta.");
-		}else {
-			System.out.println("Reprovado");
-		}
-		System.out.println("Fim do programa");
+		} else if (resultadofinal >= 40 && faltas < maxFaltas){
+            System.out.println("Recuperacao por média");
+        } else if (faltas > maxFaltas){
+            System.out.println("Reprovado por falta.");
+        }else {
+            System.out.println("Reprovado.");
+        }
+        System.out.println("Fim do program!");
     }
-}
+}        
